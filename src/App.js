be63f15899337a547c3dc95003f118867211a1c3
James Geneser <jamesgeneser@gmail.com>
2023-06-20 17:14:07 -0600

    addition to browser router

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="font-mono">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Navigation />
 
         <Routes>
