3f1ace651b2ad013b042280b677c56bfe1070257
James Geneser <jamesgeneser@gmail.com>
2023-06-23 17:18:45 -0600

    basename added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Router, Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Project from "./components/pages/Project";
 import Home from "./components/pages/Home";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 import "./App.css";
 import { createHashRouter, RouterProvider } from "react-router-dom";
@@ -18,7 +18,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
   return (
     <div className="font-mono">
-      <HashRouter>
+      <HashRouter basename="/">
         <Navigation />
 
         <div>
